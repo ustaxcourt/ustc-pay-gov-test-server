198256eb4e1d0d257c921208fe9ccdbaacd278ed
Anurag Sharma <asharma@flexion.us>
2025-09-09 12:06:52 -0700

    fixed testing and iam permissions

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -1,7 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NODE_ENV: "local" | "production";
+      NODE_ENV: "local" | "development";
       BASE_URL: string;
       BUCKET_NAME: string;
       ACCESS_TOKEN: string;
