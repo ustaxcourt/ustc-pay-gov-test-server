f43171719cbc13f533cd8c80e7c04444af455d0e
Franz Noel Tanglao <Franz.Tanglao.ctr@ustaxcourt.gov>
2026-04-29 13:43:56 -0700

    PAY-288 feat: Remove unused Complete Transaction request params

--- src/types/CompleteTransactionRequest.ts
+++ src/types/CompleteTransactionRequest.ts
@@ -1,10 +1,4 @@
 export type CompleteTransactionRequest = {
   token?: string;
   tcs_app_id?: string;
-  agency_tracking_id?: string;
-  transaction_type?: string;
-  transaction_amount?: string;
-  language?: string;
-  url_success?: string;
-  url_cancel?: string;
 };
