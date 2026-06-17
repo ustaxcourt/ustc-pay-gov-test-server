3dd8ad5623660aad6bb8e1006b86cd18014bd8c1
Mike Marcotte <mike.marcotte@gmail.com>
2026-06-17 16:58:16 -0400

    PAY-340: alternative approach introducing `EDGE_ALLOWED_CHARACTERS`

--- src/useCaseHelpers/generatePaygovTrackingId.ts
+++ src/useCaseHelpers/generatePaygovTrackingId.ts
@@ -2,18 +2,19 @@ import { randomInt } from "crypto";
 
 const ALLOWED_CHARACTERS =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ";
+const EDGE_ALLOWED_CHARACTERS = ALLOWED_CHARACTERS.replace(/ /g, "");
 
 export const generatePaygovTrackingId = (): string => {
-  const chars = Array.from(
-    { length: 21 },
+  const leading =
+    EDGE_ALLOWED_CHARACTERS[randomInt(EDGE_ALLOWED_CHARACTERS.length)];
+  const middle = Array.from(
+    { length: 19 },
     () => ALLOWED_CHARACTERS[randomInt(ALLOWED_CHARACTERS.length)],
-  );
+  ).join("");
+  const trailing =
+    EDGE_ALLOWED_CHARACTERS[randomInt(EDGE_ALLOWED_CHARACTERS.length)];
 
-  const nonSpaceLength = ALLOWED_CHARACTERS.slice(0, -1).length;
-  chars[0] = ALLOWED_CHARACTERS[randomInt(nonSpaceLength)];
-  chars[20] = ALLOWED_CHARACTERS[randomInt(nonSpaceLength)];
-
-  return chars.join("");
+  return leading + middle + trailing;
 };
 
 export const paygovTrackingIdRegex =
